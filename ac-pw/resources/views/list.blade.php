300916329c22ab6003fdbd9c70dd35402e10a828
中村圭輝 <tel07360102@gmail.com>
2020-07-23 18:27:27 +0900

    ログイン後list画面へ遷移

---
+++ ac-pw/resources/views/list.blade.php
@@ -0,0 +1,76 @@
+@extends('layouts.app')
+
+@section('content')
+
+  <div class="container">
+  	<div class="acpw">
+  		<h1>
+  			<a href="https://slack.com/intl/ja-jp/signin" target="_blank" class="sitename" class="sitename">
+  				SLACK
+  			</a>
+  		</h1>
+  		<table class="table">
+  			<tr>
+  				<th>アカウント</th>
+  				<td>account</td>
+  			</tr>
+  			<tr>
+  				<th>パスワード</th>
+  				<td>xxxx</td>
+  			</tr>
+  		</table>
+  	</div>
+	  <div class="acpw">
+	  	<h1>
+	  		<a href="https://id.heroku.com/login" target="_blank" class="sitename">
+	  			Heroku
+	  		</a>
+	  	</h1>
+	  	<table>
+	  		<tr>
+	  			<th>アカウント</th>
+	  			<td>account</td>
+	  		</tr>
+	  		<tr>
+	  			<th>パスワード</th>
+	  			<td>xxxx</td>
+	  		</tr>
+	  	</table>
+    </div>
+    <div class="acpw">
+	  	<h1>
+	  		<a href="https://id.heroku.com/login" target="_blank" class="sitename">
+	  			Heroku
+	  		</a>
+	  	</h1>
+	  	<table class="table">
+	  		<tr>
+	  			<th>アカウント</th>
+	  			<td>account</td>
+	  		</tr>
+	  		<tr>
+	  			<th>パスワード</th>
+	  			<td>xxxx</td>
+	  		</tr>
+	  	</table>
+    </div>
+    <div class="acpw">
+	  	<h1>
+	  		<a href="https://id.heroku.com/login" target="_blank" class="sitename">
+	  			Heroku
+	  		</a>
+	  	</h1>
+	  	<table class="table">
+	  		<tr>
+	  			<th>アカウント</th>
+	  			<td>account</td>
+	  		</tr>
+	  		<tr>
+	  			<th>パスワード</th>
+	  			<td>xxxx</td>
+	  		</tr>
+	  	</table>
+	  </div>
+	</div>
+
+@endsection
