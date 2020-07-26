a54395411ef2dd05e31702040a64d3c5596ef13c
中村圭輝 <tel07360102@gmail.com>
2020-07-26 13:34:34 +0900

    テーブル定義追加

---
+++ ac-pw/app/Http/Controllers/AcpwController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Acpw;
+use Illuminate\Http\Request;
+
+class AcpwController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Acpw  $acpw
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Acpw $acpw)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Acpw  $acpw
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Acpw $acpw)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Acpw  $acpw
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Acpw $acpw)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Acpw  $acpw
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Acpw $acpw)
+    {
+        //
+    }
+}
