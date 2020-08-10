f18ccfd27ffdc14670abcde4d51621d88359751a
中村圭輝 <tel07360102@gmail.com>
2020-08-10 18:29:46 +0900

    controller追加

--- ac-pw/routes/web.php
+++ ac-pw/routes/web.php
@@ -20,4 +20,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('list');
-Route::get('/add', 'HomeController@index')->name('add');
+Route::get('/add', 'AddController@index')->name('add');
